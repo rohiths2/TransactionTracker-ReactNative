f5968d8eb7bba565961788ab955e463c4d24c557
rohiths2 <rohiths2@illinois.edu>
2025-06-04 13:09:17 -0500

    added files:

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,57 +1,107 @@
 import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import { LineChart } from 'react-native-chart-kit';
+import { Alert, KeyboardAvoidingView, StatusBar } from 'react-native';
+import { Platform, StyleSheet, View, Text, ScrollView, Button, Modal, TextInput, FlatList } from 'react-native';
+import { useState } from 'react';
+import { Line } from 'react-native-svg';
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+const img = require("@/assets/images/icon.png");
 
-export default function HomeScreen() {
+export default function App() {
+  const [prevB, setPrevB] = useState(0.0);
+  const [prevT, setPrevT] = useState(0.0);
+  const [balance, setBalance] = useState(0.0);
+  const [transactionTotal, setTransactionTotal] = useState(0.0);
+  const [temp, setTemp] = useState("");
+  const [transactionHistory, setTransactionHistory] = useState(['Transaction History:'])
+  const [balanceHistory, setBalanceHistory] = useState([0])
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <ScrollView style={{flex: 1, backgroundColor: "cyan"}}>
+      <View>
+        <Text style={{fontFamily: "Comic", paddingTop: 40, fontSize: 20, textAlign: 'center'}}>Transaction Tracker</Text>
+      </View>
+      <View>
+        <Text style={{fontFamily: "Comic", padding: 10, fontSize: 20, textAlign: 'center'}}>Account balance: ${balance}</Text>
+        <Text style={{fontFamily: "Comic", fontSize: 20, textAlign: 'center'}}>Total spent: ${transactionTotal}</Text>
+      </View>
+      <View style={styles.container}>
+        <Text>Transaction: (numeric only, max 2 decimal places)</Text>
+        <TextInput onChangeText={ (text) => {
+          // Source for RegEx used below: https://stackoverflow.com/questions/68784025/how-to-prevent-the-input-field-from-accepting-more-than-2-decimal-places-in-reac
+            if (text.match(/^(\d*\.{0,1}\d{0,2}$)/)) {
+              setTemp(text)
+            }
+          }
+          } keyboardAppearance='dark' value = {temp} placeholder = 'Enter amount here' style={{borderColor: 'black', borderWidth: 1}}></TextInput>
+        <Button title='I spent this much' onPress={() => {
+          if (temp != "") {
+            setPrevB(balance)
+            setPrevT(transactionTotal)
+            setTransactionTotal(transactionTotal + parseFloat(temp))
+            setBalance(balance - parseFloat(temp))
+            setBalanceHistory(balanceHistory.concat([balance]))
+            const day = new Date().getDate()
+            const month = new Date().getMonth()
+            const year = new Date().getFullYear()
+            setTransactionHistory(transactionHistory.concat(['On ' + (month+1) + '/' + day + '/' + year + ': Spent $' + temp]))
+          }
+        }}></Button>
+                <Button title='I earned this much' onPress={() => {
+          if (temp != "") {
+            setPrevB(balance)
+            setBalance(balance + parseFloat(temp))
+            setBalanceHistory(balanceHistory.concat([balance]))
+            const day = new Date().getDate()
+            const month = new Date().getMonth()
+            const year = new Date().getFullYear()
+            setTransactionHistory(transactionHistory.concat(['On ' + (month+1) + '/' + day + '/' + year + ': Earned $' + temp]))
+          }
+        }}></Button>
+        <Button title='Undo last transaction' onPress={
+        () => Alert.alert("Undo last transaction", "Are you sure you want to undo the last change to your Balance and/or Total (reverse the last spending/transaction entry)?", [{
+            text: 'Yes',
+            onPress: ()  => {
+              setBalance(prevB)
+              setTransactionTotal(prevT)
+              setBalanceHistory(balanceHistory.slice(0, balanceHistory.length - 1))
+              setTransactionHistory(transactionHistory.slice(0, transactionHistory.length - 1))
+            }
+        },
+      {
+        text: 'No'
+      }])
+      }></Button>
+                        <Button title='Reset' onPress={
+        () => Alert.alert("Reset to 0", "Are you sure you want to change Balance and Total Spent both to zero?", [{
+            text: 'Yes',
+            onPress: ()  => {
+              setPrevB(balance)
+              setPrevT(transactionTotal)
+              setBalance(0)
+              setTransactionTotal(0)
+              setTransactionHistory(['Transaction History:'])
+              setBalanceHistory([0])
+            }
+        },
+      {
+        text: 'No'
+      }])
+      }></Button>
+      </View>
+      <ScrollView>
+        <FlatList data={transactionHistory} renderItem={
+          ({item}) => (
+            <Text style={{textAlign: 'center'}}>{item}</Text>
+          )
+        }>
+        </FlatList>
+      </ScrollView>
+      <View>
+
+      </View>
+      <View style={{height: 100}}>
+      </View>
+    </ScrollView>
   );
 }
 
@@ -59,11 +109,14 @@ const styles = StyleSheet.create({
   titleContainer: {
     flexDirection: 'row',
     alignItems: 'center',
-    gap: 8,
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  container: {
+    width: 250,
+    alignSelf: 'center',
+    backgroundColor: 'white',
+    justifyContent: "center",
+    paddingHorizontal: 20,
+    paddingVertical: 20,
   },
   reactLogo: {
     height: 178,
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -10,6 +10,7 @@ export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    Comic: require('@/assets/fonts/comic.ttf'),
   });
 
   if (!loaded) {
